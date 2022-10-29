b9c892706a20b06478a0b3e9d0ec0e3fbd3943e0
Ifegwu <agbanyimdaniel29@gmail.com>
2022-10-29 18:52:24 +0200

    wrote test for test_train_models function

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -9,7 +9,6 @@ Date: 28th October 2022
 # Import libaries
 import os
 import logging
-from pickle import TRUE
 import churn_library as clib
 from math import ceil
 
@@ -182,7 +181,7 @@ def test_encoder_helper():
 
 def test_perform_feature_engineering():
     '''
-    test perform_feature_engineering
+    test perform_feature_engineering function from churn_library module
     '''
 
     # Load the dataframe
@@ -207,7 +206,7 @@ def test_perform_feature_engineering():
         raise err
 
     try:
-        # X_test size should be 30# of `dataframe`
+        # X_test size should be 30% of `dataframe`
         assert (X_test.shape[0] == ceil(dataframe.shape[0]*0.3)) is True
         logging.info(
             'Testing perform_feature_engineering. DataFrame sizes are consistent: SUCCESS')
@@ -219,8 +218,70 @@ def test_perform_feature_engineering():
 
 def test_train_models(train_models):
     '''
-    test train_models
+    test train_models function from churn_library module
     '''
+    # Load the dataframe
+    dataframe = clib.import_data("./data/bank_data.csv")
+
+    # Churn feature
+    dataframe['Churn'] = dataframe['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1
+    )
+
+    # Feature engineering
+    (X_train, X_test, y_train, y_test) = clib.perform_feature_engineering(
+        dataframe=dataframe,
+        response='Churn')
+
+    # Assert that `logistic_model.pkl` file exists
+    try:
+        clib.train_models(X_train, X_test, y_train, y_test)
+        assert os.path.isfile("./models/logistic_model.pkl") is True
+        logging.info('File %s was found', 'logistic_model.pkl')
+    except AssertionError as err:
+        logging.error('No such file on the disk')
+        raise err
+
+    # Assert that `rfc_model.pkl` file exist
+    try:
+        assert os.path.isfile("./models/rfc_model.pkl") is True
+        logging.info('File %s was found', 'rfc_model.pkl')
+    except AssertionError as err:
+        logging.error('No such file on disk')
+        raise err
+
+    # Assert that `roc_curve_result.png` file is present
+    try:
+        assert os.path.isfile('./images/results/roc_curve_result.png') is True
+        logging.info('File %s was found', 'roc_curve_result.png')
+    except AssertionError as err:
+        logging.error('No such file on disk')
+        raise err
+
+    # Assert that `rfc_results.png` file is present
+    try:
+        assert os.path.isfile('./images/results/rf_results.png') is True
+        logging.info('File %s was found', 'rf_results.png')
+    except AssertionError as err:
+        logging.error('No such file on disk')
+        raise err
+
+    # Assert that `logistic_results.png` file is present
+    try:
+        assert os.path.isfile('./images/results/logistic_results.png') is True
+        logging.info('File %s was found', 'logistic_results.png')
+    except AssertionError as err:
+        logging.error('No such file on disk')
+        raise err
+
+    # Assert that `feature_importances.png` file is present
+    try:
+        assert os.path.isfile(
+            './images/results/feature_importances.png') is True
+        logging.info('File %s was found', 'feature_importances.png')
+    except AssertionError as err:
+        logging.error('No such file on disk')
+        raise err
 
 
 if __name__ == "__main__":
@@ -228,3 +289,4 @@ if __name__ == "__main__":
     test_eda()
     test_encoder_helper()
     test_perform_feature_engineering()
+    test_train_models()
